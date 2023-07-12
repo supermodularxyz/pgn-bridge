8b8f8be91169c2d40aac39759aaaf71e1ca2d2b7
Carl Barrdahl <carlbarrdahl@gmail.com>
2023-07-12 13:57:06 +0200

    Fix build

--- config/tokens.ts
+++ config/tokens.ts
@@ -7,17 +7,18 @@ export const tokens = [
     l1Address: "",
     l2Address: "",
   },
-  networks.l2.network === "pgnTestnet"
-    ? {
-        name: "TestToken",
-        l1Address: "0x10246FE5Bf3b06Fc688eD4AA1445FF52358CE3A9",
-        l2Address: "0x7c6b91D9Be155A6Db01f749217d76fF02A7227F2",
-      }
-    : null,
 ];
 
+if (networks.l2.network === "pgnTestnet") {
+  tokens.push({
+    name: "TestToken",
+    l1Address: "0x10246FE5Bf3b06Fc688eD4AA1445FF52358CE3A9",
+    l2Address: "0x7c6b91D9Be155A6Db01f749217d76fF02A7227F2",
+  });
+}
+
 export const getToken = (addr?: Address) =>
-  tokens.find((t) => t.l1Address === addr);
+  tokens.find((t) => t?.l1Address === addr);
 
 export const getTokenForChain = (addr: Address, network: string) =>
   getToken(addr)?.[
