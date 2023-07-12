50d776e19cef2c6516a90ad330a93660553ecd6b
Carl Barrdahl <carlbarrdahl@gmail.com>
2023-07-12 13:54:10 +0200

    Only show TestToken on testnet

--- config/tokens.ts
+++ config/tokens.ts
@@ -1,4 +1,5 @@
 import { Address } from "wagmi";
+import { networks } from "./networks";
 
 export const tokens = [
   {
@@ -6,11 +7,13 @@ export const tokens = [
     l1Address: "",
     l2Address: "",
   },
-  {
-    name: "TestToken",
-    l1Address: "0x10246FE5Bf3b06Fc688eD4AA1445FF52358CE3A9",
-    l2Address: "0x7c6b91D9Be155A6Db01f749217d76fF02A7227F2",
-  },
+  networks.l2.network === "pgnTestnet"
+    ? {
+        name: "TestToken",
+        l1Address: "0x10246FE5Bf3b06Fc688eD4AA1445FF52358CE3A9",
+        l2Address: "0x7c6b91D9Be155A6Db01f749217d76fF02A7227F2",
+      }
+    : null,
 ];
 
 export const getToken = (addr?: Address) =>
